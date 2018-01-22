7f495839ee153d68ff60d251780e6fb26662af6e
Arjun S <arjun921@gmail.com>
2018-01-22 08:54:53 +0530

    Update

--- app.py
+++ app.py
@@ -9,10 +9,11 @@ def load_page():
     webpage = urlopen(req).read()
     return webpage
 
-
+count = 0
 while True:
+    count +=1
     print("Sleep ended")
     print(load_page())
     print("Web page called")
-    print("Sleep started...")
+    print("Sleep started... {}".format(count))
     time.sleep(1200)
