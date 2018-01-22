a9a1c13487d80b1e49a156748ee9c6ff87da29f5
Arjun S <arjun921@gmail.com>
2018-01-22 08:14:24 +0530

    Update

---
+++ app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from urllib.request import Request, urlopen
+import time
+
+
+def load_page():
+    req = Request('https://rajasthan-aanganwadi.herokuapp.com', headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(req).read()
+    return webpage
+
+
+while True:
+    print("Sleep ended")
+    load_page()
+    print("Web page called")
+    print("Sleep started...")
+    time.sleep(1200)
